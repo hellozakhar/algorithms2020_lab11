29b8bc136116a51e96f75b8adaf2dfff2f88125c
hellozakhar <jaguar7894@yandex.ru>
2020-12-24 21:52:25 +0300

    rewrite "findRK" function

--- Algorithms2020_lab11/Algorithms2020_lab11.cpp
+++ Algorithms2020_lab11/Algorithms2020_lab11.cpp
@@ -21,7 +21,7 @@ int random(const int a, const int b)
 
 std::string random_text(const char(*src), const size_t len)
 {
-	const int N = strlen(src);
+	const size_t N = strlen(src);
 	const size_t n = 1 + len / N;
 
 	std::basic_string<char> text = src;
@@ -69,104 +69,65 @@ std::string::size_type findKMP(std::string const& str, int const& begin, std::st
 	return (std::string::npos);
 }
 
-long long polyhash(std::string const& s, int const& lower, int const& upper) {
-	const int a = 31;
-	const int p = 1e9 + 9;
-	long long hash_value = 0;
-	long long a_pow = 1;
-
-	//std::cout << s.substr(lower, upper - lower) << std::endl;
-	
-	for (int i = lower; i <= upper; i++) {
-		hash_value = (hash_value + (s[i] - 'a' + 1) * a_pow) % p;
-		
-		//std::cout << "s[" << i << "] = " << s[i] << std::endl;
-		a_pow = (a_pow * a) % p;
-	}
-	//std::cout << std::endl;
-//	std::cout << "polyhash(" << s.substr(lower, upper - lower) << ")" << " = " << hash_value << std::endl;
-	return hash_value;
-}
-
-long long polyhash(std::string const& s) {
-	return polyhash(s, 0, s.size() - 1);
-}
-
-long long polyhash(std::string const& s, int const& lower, int const& upper, long long const& previous_hash) {
-	
-	if (previous_hash == 0) {
-		return polyhash(s, lower, upper);
-	}
-	
-	const int a = 31;
-	const int p = 1e9 + 9;
-	long long a_pow = 1;
-	long long hash_value = 0;
-
-	// delete hash of first char from previous string
-	// (s[lower-1] - 'a' + 1)  <--- hash of first char
-	// example
-	// A [B C D E]
-	// ^
-	// |
-	// A - first char from previous string ("[A B C D]")
-	
-	hash_value = (previous_hash - (s[lower-1] - 'a' + 1));
-	hash_value /= a;
-
-	// calculating a^(...) for right char of string
-	for (int i = 0; i < upper - lower; i++) {
-		/*a_pow *= a;
-		a_pow %= p;*/
-		a_pow = (a_pow * a) % p;
-	}
-
-	//  A [B C D E]
-	//           ^
-	//           |
-	// add hash right char
-	//              |
-	
-	hash_value += ((s[upper] - 'a' + 1) * a_pow) % p;
-
-	return hash_value;
-}
-		
 std::string::size_type findRK(std::string const& str, std::string const& pattern) {
 
-	const int str_size = str.length();
-	const int pattern_size = pattern.length();
-	const long long pattern_hash = polyhash(pattern);
-	long long current_hash = 0;
+	const size_t str_size = str.length();
+	const size_t pattern_size = pattern.length();
 
-	for (int i = 0; i < str_size - pattern_size + 1; i++) {
+	// p = 2^61 - 1 or p = 2^31 - 1
+	const unsigned long long p = ((unsigned long long) pow(2, 61)) - 1ULL;
+	// a in {0, ..., p - 1}
+	const unsigned long long a = 2ULL;
 
-		current_hash = polyhash(str, i, i + pattern_size - 1, current_hash);
+	// precomp positive a: { 1, a^1, a^2, ..., a^n }
+	// complexity O(n)
+	unsigned long long* apos = new unsigned long long[str_size + 1];
+	apos[0] = 1ULL;
+	for (int i = 1; i <= str_size; i++) {
+		apos[i] = (apos[i - 1] << 1) % p;
+		std::cout << "apos[" << i << "]= " << apos[i] << std::endl;
+	}
+
+	// precomp H_i, where H0 is empty str
+	// H0 = h(T[0,0)) = 0, H1 = h(T[0, 1)), ...
+	unsigned long long* H = new unsigned long long[str_size + 1];
+	H[0] = 0ULL;
+	for (int i = 0; i < str_size; i++) {
+	    H[i + 1] = (H[i] + str[i] * apos[i]) % p;
+		std::cout << "H[" << i + 1 << "]= " << H[i + 1] << std::endl;
+	}
+
+	// calculating hash of pattern
+	unsigned long long pattern_hash = 0ULL;
+	for (int i = 0; i < pattern_size; i++) {
+		//pattern_hash = (pattern_hash + pattern[i] * apos[i]) % p;
+		pattern_hash += (pattern[i] << i) % p;
+	}
+
+	unsigned long long h = 0ULL;
+	for (int i = 0; i < str_size - pattern_size; i++) {
 		
-		std::cout << "\ncurrent_string = <" << str.substr(i, pattern_size) << ">" << std::endl;
-		std::cout << "i: " << i << "\ns[lower] = " << str[i] << ", s[upper] = " 
-			      << str[i + pattern_size - 1] << ", " << std::endl;
-		std::cout << "\ncurrent_hash = " << current_hash << std::endl;
-		std::cout << "expectedhash = " << polyhash(str, i, i + pattern_size - 1, 0) << std::endl;
-		/*std::cout << "current_hash_expected = " << polyhash(str, i, i + pattern_size - 1) << std::endl;
-		std::cout << "current_hash_expected = " << polyhash(str.substr(i, pattern_size)) << std::endl;*/
+		// h = ((unsigned long long)((H[i + pattern_size] - H[i]) / apos[i])) % p;
+		h = (((H[i + pattern_size] - H[i]) >> i)) % p;
 
-		if (current_hash == pattern_hash) {
-			 //and now checking strings
+		if (h == pattern_hash) {
 			if (str.substr(i, pattern_size) == pattern) {
-				 //substr found!
+				delete[] apos;
+				delete[] H;
 				return i;
 			}
 		}
 	}
-
+	
+	delete[] apos;
+	delete[] H;
 	return (std::string::npos);
 }
 
 std::string::size_type findNaive(std::string const& str, std::string const& pattern) {
 
-	const int str_size = str.length();
-	const int pattern_size = pattern.length();
+	const size_t str_size = str.length();
+	const size_t pattern_size = pattern.length();
 
 	for (int i = 0; i <= str_size - pattern_size; ++i) {
 		bool correct = true;
@@ -191,16 +152,16 @@ void test() {
 	const std::string str_example_1 = "fkdsaf  o,i.e feojwaofk fndsan  nfdsna kklfdsa neoiwfn qi,f e ewqef  hdffds zakh fmdska ieow ks n cdaddc. dskj fnd;akjnf d;sanf;eiwajnf oeiwja foiewjaf oidsjaf oiejwqfoenwqfjnewq'ojfnewq'lkrnewqkfnjvofi[djsboifjdsgjrnewkgjenr;kgjernw;kfjnew;kfjnresodxbjf'objfr'eowgn";
 
 	const std::string string_example03 = str_example_1.substr(0, 70);
-	const std::string pattern_example_03 = str_example_1.substr(20, 6);
+	const std::string pattern_example_03 = str_example_1.substr(60, 6);
 	
 	std::cout << "string_example_3: " << string_example03 << std::endl;
 	std::cout << "pattern_example_3: " << pattern_example_03 << std::endl;
 	std::cout << "pattern_length: " << pattern_example_03.size() << std::endl;
-	std::cout << "polyhash: " << polyhash(pattern_example_03) << std::endl;
+	//std::cout << "polyhash: " << polyhash(pattern_example_03) << std::endl;
 	
-	std::cout << std::endl;
-	std::cout << findRK(string_example03, pattern_example_03) << std::endl;
-	std::cout << findNaive(string_example03, pattern_example_03) << std::endl;
+	std::cout << "findRK start ---" << std::endl;
+	std::cout << "findRK result: " << findRK(string_example03, pattern_example_03) << std::endl;
+	std::cout << "findNaive result: " << findNaive(string_example03, pattern_example_03) << std::endl;
 }
 
 void testFind(int const& times) {
@@ -219,8 +180,8 @@ void testFind(int const& times) {
 	int maxlen = 0;
 	std::string generatedstr = "";
 	std::string pattern = "";
-	int len_generatedstr = 0;
-	int len_pattern = 0;
+	size_t len_generatedstr = 0;
+	size_t len_pattern = 0;
 	int lower = 0;
 	//int upper = 0;
 	int resultKMP = 0;
@@ -268,8 +229,9 @@ void testFind(int const& times) {
 
 			if (k == 0) {
 				if (resultKMP != resultRK || resultRK != resultNaive) {
-					/*std::cout << "Wrong result! Results: {" << resultKMP
+					std::cout << "Wrong result! Results: {" << resultKMP
 						      << ", " << resultRK << ", " << resultNaive << '}' << std::endl;
+					/*
 					std::cout << "\nString: " << generatedstr << std::endl;
 					std::cout << "\nPattern: " << pattern << std::endl;
 					std::cout << "\npolyhash(Pattern): " << polyhash(pattern) << std::endl;
@@ -551,5 +513,191 @@ int main()
 	test();
 	//testFind(times);
 	//testFindFiction(times);
+	std::cout << pow(2, 61);
     std::cout << "\nHello World!\n";
 }
+
+//int pos = 61;
+//h = ((unsigned long long) ((H[pos + pattern_size] - H[pos]) / apos[pos])) % p;
+////h = (((H[pos + pattern_size] - H[pos]) >> pos)) % p;
+//
+//std::cout << "\nH[26]: " << H[26] << std::endl;
+//std::cout << "\nH[21]: " << H[21] << std::endl;
+//std::cout << "\nH[26] - H[21]: " << (H[26] - H[21]) << std::endl;
+//
+//std::cout << "\nh: " << h << std::endl;
+//std::cout << "\nRK3-pattern_hash: " << pattern_hash << std::endl;
+//
+//unsigned long long tmp = 330911306547813444ULL;
+//unsigned long long rlt = tmp >> 5;
+//std::cout << "tmp: " << tmp << std::endl;
+//std::cout << "rlt: " << rlt << std::endl;
+//std::cout << 'b' + 0 << std::endl;
+//
+//unsigned long long testr = 0;
+//char* eptr = new char('b');
+//testr = strtoull(eptr, &eptr, 1);
+//std::cout << "testr: " << testr << std::endl;
+//
+
+//long long polyhash(std::string const& s, int const& lower, int const& upper) {
+//	const int a = 31;
+//	const int p = 1e9 + 9;
+//	long long hash_value = 0;
+//	long long a_pow = 1;
+//
+//	//std::cout << s.substr(lower, upper - lower) << std::endl;
+//	
+//	for (int i = lower; i <= upper; i++) {
+//		// hash_value = (hash_value + (s[i] - 'a' + 1) * a_pow) % p;
+//		hash_value = (hash_value + s[i] * a_pow) % p;
+//		
+//		//std::cout << "s[" << i << "] = " << s[i] << std::endl;
+//		a_pow = (a_pow * a) % p;
+//	}
+//	//std::cout << std::endl;
+////	std::cout << "polyhash(" << s.substr(lower, upper - lower) << ")" << " = " << hash_value << std::endl;
+//	return hash_value;
+//}
+//
+//long long polyhash(std::string const& s) {
+//	return polyhash(s, 0, s.size() - 1);
+//}
+//
+//long long polyhash(std::string const& s, int const& lower, int const& upper, long long const& previous_hash) {
+//	
+//	if (previous_hash == 0) {
+//		return polyhash(s, lower, upper);
+//	}
+//	
+//	const int a = 31;
+//	const int p = 1e9 + 9;
+//	long long a_pow = 1;
+//	long long hash_value = 0;
+//
+//	// delete hash of first char from previous string
+//	// (s[lower-1] - 'a' + 1)  <--- hash of first char
+//	// example
+//	// A [B C D E]
+//	// ^
+//	// |
+//	// A - first char from previous string ("[A B C D]")
+//	
+//	hash_value = (previous_hash - (s[lower-1] - 'a' + 1));
+//	hash_value /= a;
+//
+//	// calculating a^(...) for right char of string
+//	for (int i = 0; i < upper - lower; i++) {
+//		/*a_pow *= a;
+//		a_pow %= p;*/
+//		a_pow = (a_pow * a) % p;
+//	}
+//
+//	//  A [B C D E]
+//	//           ^
+//	//           |
+//	// add hash right char
+//	//              |
+//	hash_value += ((s[upper] - 'a' + 1) * a_pow) % p;
+//
+//	return hash_value;
+//}
+//		
+//std::string::size_type findRK(std::string const& str, std::string const& pattern) {
+//
+//	const int str_size = str.length();
+//	const int pattern_size = pattern.length();
+//	const long long pattern_hash = polyhash(pattern);
+//	long long current_hash = 0;
+//
+//	std::ofstream o;
+//	o.open("findRK.log", std::ios::app);
+//	o << "\nstring = <" << str << ">" << ", length = " << str.length() << std::endl;
+//	o << "pattern = <" << pattern << ">" << ", length = " << pattern.length() << std::endl;
+//	   
+//	for (int i = 0; i < str_size - pattern_size + 1; i++) {
+//
+//		current_hash = polyhash(str, i, i + pattern_size - 1, current_hash);
+//		
+//		std::cout << "i: " << i << "\ns[lower] = " << str[i] << ", s[upper] = "
+//			<< str[i + pattern_size - 1] << ", " << std::endl;
+//		std::cout << "current_string = <" << str.substr(i, pattern_size) << ">" << std::endl;
+//		std::cout << "current_hash = " << current_hash << std::endl;
+//		std::cout << "expectedhash = " << polyhash(str, i, i + pattern_size - 1, 0) << '\n' << std::endl;
+//
+//		o << "i: " << i << "\ns[lower] = " << str[i] << ", s[upper] = "
+//			<< str[i + pattern_size - 1] << ", " << std::endl;
+//		o << "current_string = <" << str.substr(i, pattern_size) << ">" << std::endl;
+//		o << "current_hash = " << current_hash << std::endl;
+//		o << "expectedhash = " << polyhash(str, i, i + pattern_size - 1, 0) << '\n' << std::endl;
+//		/*std::cout << "current_hash_expected = " << polyhash(str, i, i + pattern_size - 1) << std::endl;
+//		std::cout << "current_hash_expected = " << polyhash(str.substr(i, pattern_size)) << std::endl;*/
+//
+//		if (current_hash == pattern_hash) {
+//			 //and now checking strings
+//			if (str.substr(i, pattern_size) == pattern) {
+//				 //substr found!
+//				return i;
+//			}
+//		}
+//	}
+//	o.close();
+//
+//	return (std::string::npos);
+//}
+
+//long long h(std::string const& str) {
+//	const int a = 29;
+//	const int p = 31;
+//	long long hash = 0;
+//
+//	for (int i = 0; i < str.size(); i++) {
+//		hash += str[i] * pow(a, i);
+//	}
+//	hash = hash % p;
+//	return hash;
+//}
+
+//long long h(std::string const& str, long long& prev_hash) {
+//	if (prev_hash == 0) {
+//		return h(str);
+//	}
+//
+//	const int a = 29;
+//	const int p = 31;
+//	long long hash = 0;
+//
+//	hash = prev_hash + str[str.size() - 1];
+//
+//}
+
+
+//std::string::size_type findRK2(std::string const& str, std::string const& pattern) {
+//	const int str_size = str.length();
+//	const int pattern_size = pattern.length();
+//	const long long pattern_hash = 0;
+//	const int hashCount = str_size - pattern_size;
+//	long long* hashArr = new long long[hashCount];
+//
+//	for (int i = 0; i < hashCount; i++) {
+//		if (i == 0) {
+//			hashArr[i] = h(str.substr(i, pattern_size));
+//		}
+//		else {
+//			hashArr[i] = h(str.substr(i, pattern_size), hashArr[i-1]);
+//		}
+//		
+//	}
+//
+//	for (int i = 0; i < hashCount; i++) {
+//		if (hashArr[i] == pattern_hash) {
+//			if (str.substr(i, pattern_size) == pattern) {
+//				delete[] hashArr;
+//				return i;
+//			}
+//		}
+//	}
+//
+//	delete[] hashArr;
+//	return (std::string::npos);
+//}
